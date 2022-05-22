b1aa01ac07b52624462409cadaa230e56363b2bc
ArtakOvakimyan <71792261+ArtakOvakimyan@users.noreply.github.com>
2022-05-22 09:03:34 +0500

    Updated main.py

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-"""
-This script will provide you to compare different methods of searching a substring in a text on various texts.
-The result will be shown on your terminal screen :)
-
-To start to use:
-    1)Print your text or main-string in file in.txt
-    2)Run script by command in terminal: python3 main.py
-    3)Print substring in terminal
-"""
-from algorithms_store import algorithm
+import argparse
+from algorithms_handler import Handler
+from text import String
 
 
 def main():
-    a = algorithm()
-    a.perform_all()
+    parser = argparse.ArgumentParser(
+        usage="$ python3 [Main-text file_name] {SCRIPT file_name}",
+        description="""This script will provide you to compare different methods of searching a substring.
+                        The result will be shown on your terminal screen.
+
+                        To start to use:
+                            1)Print your text or main-string in file "in.txt"
+                            2)Run script by command in terminal: python3 main.py
+                            3)Print substring in terminal
+                     """
+    )
+    parser.add_argument("file_path", type=str)
+    args = parser.parse_args()
+    h = Handler(args.file_path, String(input("Введите подстроку: ")))
+    h.perform_all()
 
 
 if __name__ == "__main__":
     main()
+
